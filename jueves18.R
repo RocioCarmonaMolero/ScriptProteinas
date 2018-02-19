1d9bf3b833d8cc673f24a9032dea81234938ae74
Rocio Carmona Molero <36073887+RocioCarmonaMolero@users.noreply.github.com>
2018-02-19 20:42:24 +0100

    Delete jueves18.R

--- jueves18.R
+++
@@ -1,6 +0,0 @@
-
-
-
-#leer los datos, que no tome ninguna linea como header
-dat <-  read.csv("6E70ADJ0014-Alignment-HitTable.csv", header = FALSE, stringsAsFactors = F)
-#sacar segunda columna, la que interesa (devuelve factores)
