708299240753094989a70f655bb968327cdc9ec9
Rocio Carmona Molero <36073887+RocioCarmonaMolero@users.noreply.github.com>
2018-03-14 10:53:33 +0100

    Create get_mol_weight.R

---
+++ get_mol_weight.R
@@ -0,0 +1,8 @@
+weight_prot <- function(lca)
+  
+lca <- strsplit(entrez_fetch(db="protein", id=xp, rettype='gp'), "\n")
+for(i in seq_along(lca[[1]])){
+  if(grepl("calculated_mol_wt", lca[[1]][i]) == TRUE){
+    mol_wg <- strsplit(lca[[1]][i], "=")[[1]][2]
+  }
+}
