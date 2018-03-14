db5d87031fd2e381f96ff5b288a283371a439ba6
Rocio Carmona Molero <36073887+RocioCarmonaMolero@users.noreply.github.com>
2018-03-14 11:00:20 +0100

    Update get_mol_weight.R

--- get_mol_weight.R
+++ get_mol_weight.R
@@ -1,8 +1,8 @@
 weight_prot <- function(xp)
   
-lca <- strsplit(entrez_fetch(db="protein", id=xp, rettype='gp'), "\n")
-for(i in seq_along(lca[[1]])){
-  if(grepl("calculated_mol_wt", lca[[1]][i]) == TRUE){
-    mol_wg <- strsplit(lca[[1]][i], "=")[[1]][2]
+  lca <- strsplit(entrez_fetch(db="protein", id=xp, rettype='gp'), "\n")
+  for(i in seq_along(lca[[1]])){
+    if(grepl("calculated_mol_wt", lca[[1]][i]) == TRUE){
+      mol_wg <- strsplit(lca[[1]][i], "=")[[1]][2]
+    }
   }
-}
