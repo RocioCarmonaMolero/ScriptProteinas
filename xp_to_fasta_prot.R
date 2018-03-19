7271a4832f542afae6855810a750a6500063b704
Rocio Carmona Molero <36073887+RocioCarmonaMolero@users.noreply.github.com>
2018-03-19 19:54:38 +0100

    Update xp_to_fasta_prot.R

--- xp_to_fasta_prot.R
+++ xp_to_fasta_prot.R
@@ -1,9 +1,11 @@
-xp_to_fasta_prot <- function(candidatos)
-
-library(rentrez)
-
-for(i in seq_along(candidatos)){
-  xp <- candidatos[i]
-  imp_aa_seq <- entrez_fetch(db="protein", id=xp, rettype='fasta')
-  write(imp_aa_seq, "xp_to_fasta_candidatos.fasta", append =TRUE)
+xp_to_fasta_prot <- function(data, nameFile){
+  
+  for(i in seq_along(data)){
+    xp <- data[i]
+    imp_aa_seq <- entrez_fetch(db="protein", id=xp, rettype='fasta')
+    write(imp_aa_seq, paste0(nameFile, ".fasta"), append =TRUE)
+  }
 }
+
+# nameFile in character string format "nameFile"
+# data is a vector which elements are the accesion name of proteins
