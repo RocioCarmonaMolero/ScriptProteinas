9ed91e8aa3a4f6b755280e623a81b73bc4870ff7
Rocio Carmona Molero <36073887+RocioCarmonaMolero@users.noreply.github.com>
2018-06-11 15:58:56 +0200

    Update get_mol_weight.R

--- get_mol_weight.R
+++ get_mol_weight.R
@@ -10,3 +10,19 @@ weight_prot <- function(xp){
   }
   mol_wg
 }
+
+?
+
+weight_prot <- function(xp){
+  mol_wg <- c()
+  lca <- strsplit(entrez_fetch(db="protein", id=xp, rettype='gp'), "\n")
+  for(i in seq_along(lca[[1]])){
+    if(grepl("calculated_mol_wt", lca[[1]][i]) == TRUE){
+      mol_wg <- strsplit(lca[[1]][i], "=")[[1]][2]
+    }
+    if((i == length(lca[[1]])) & (is.null(mol_wg))){
+      mol_wg <- c("a mano")
+    }
+  }
+  mol_wg
+}
